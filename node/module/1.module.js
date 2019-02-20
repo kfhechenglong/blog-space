2f07229ede39786869fb49188a5689e176914340
kfhechenglong@126.com <kfhechenglong@126.com>
2019-02-20 22:16:53 +0800

    2.20

--- node/module/1.module.js
+++ node/module/1.module.js
@@ -6,3 +6,10 @@
 * 4 nodejs common.js
 * 5 es6 es module
 */
+
+// node 原生的模块是内置在node.exe中，
+// 它的加载速度是最快的
+let fs = require('fs');
+//另一种是文件模块，是存放在硬盘中，加载速度较慢；
+// 文件模块分为三种js、json、node三种类型
+// 又分为两种，一种是自己写的，根据路径找，另一种是别人写的第三方模块通过名字去（node module中找）
