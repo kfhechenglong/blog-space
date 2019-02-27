efeb826c2963ee9c19ec39a07b9184004f9ac78f
hechenglong <kfhechenglong@126.com>
2019-02-27 13:43:12 +0800

    jsonp

--- 跨域解决方案/jsonp/jsonp.js
+++ 跨域解决方案/jsonp/jsonp.js
@@ -1,26 +1,29 @@
+
 /**
  * Created by he on 2019/2/26.
  */
-function jsonp({url,params,callbcak}){
+function jsonp({url,params,callback}){
   return new Promise((resolve,reject) => {
-    let jsonp_script = document.createElement('srcipt');
-    window[callbcak] = function (data) {
+    let jsonp_script = document.createElement('script');
+    window[callback] = function (data) {
       resolve(data);
       document.body.removeChild(jsonp_script);
     };
-    params = {...params,callbcak};
+    params = {...params,callback};
     let params_arr = [];
     for(let key in params) {
       params_arr.push(`${key}=${params[key]}`);
     }
-    jsonp_script.src = `${url}?&${params_arr.join('&')}`;
+    let src = `${url}?${params_arr.join('&')}`;
+    jsonp_script.setAttribute('src', src);
     document.body.appendChild(jsonp_script);
   })
 };
 jsonp({
-  url:'http:localhost:3000/test',
+  url:'http://localhost:3000/test',
   params:{wd:1234567},
-  callbcak:'callback_test'
+  callback:'callback_test'
 }).then(res => {
   console.log(res);
 })
+
--- 跨域解决方案/jsonp/server.js
+++ 跨域解决方案/jsonp/server.js
@@ -2,12 +2,12 @@
  * Created by he on 2019/2/26.
  */
 //创建一个本地服务
-let express = require('express');
-let app = express();
-app.get('/test',function(req,res) {
-  let {wd,callback} = req.query;
-  console.log(wd);
-  console.log(callback);
-  res.send(`${callback} 这是响应的内容，你看着处理吧！`);
-});
-app.listen(3000);
+let express = require('express')
+let app = express()
+app.get('/test', function (req, res) {
+  let { wd, callback } = req.query
+  console.log(wd)
+  console.log(callback)
+  res.end(`${callback}('这是测试的内容！')`)
+})
+app.listen(3000)
