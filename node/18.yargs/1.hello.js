0e45105749fbb7b39a17e3f134180fe69b2b7e1a
kfhechenglong@126.com <kfhechenglong@126.com>
2019-03-17 11:49:18 +0800

    http

---
+++ node/18.yargs/1.hello.js
@@ -0,0 +1,2 @@
+console.log('holle!' + process.argv[2]);
+console.log(process.argv);
---
+++ node/19.static/bin/www.js
@@ -0,0 +1,31 @@
+/*
+ -d --root 静态文件目录
+ -o --host 主机
+ -p --port 端口号
+*/
+let yargs = require('yargs');
+let Server = require('../src/app.js')
+let argv = yargs.options('d',{
+    alias:'root',
+    demand:'false',
+    type:'string',
+    default:process.cwd(),
+    description:'静态文件根目录'
+}).options('o',{
+    alias: 'host',
+    demand: 'false',
+    type: 'string',
+    default: 'localhost',
+    description: '请配置监听的主机'
+}).options('p', {
+    alias: 'port',
+    demand: 'false',
+    type: 'number',
+    default: 8080,
+    description: '请配置端口号'
+}).usage('server [options]')
+.example(
+    'server -d / -p 8080 -o localhost'
+).help('h').argv;
+let server = new Server();
+server.start();
