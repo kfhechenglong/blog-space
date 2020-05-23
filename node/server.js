14707fd6062ad1f8ece85c931e29124767b850c4
hechenglong <kfhechenglong@126.com>
2020-05-23 15:38:42 +0800

    http实现静态文件访问

--- node/server.js
+++ node/server.js
@@ -1,14 +1,27 @@
 const qs = require('querystring')
-
+const fs = require('fs');
 require('http').createServer(function(req,res) {
-    var body = '';
-    req.on('data',function(chunk) {
-        body += chunk;
-    })
-    req.on('end',function() {
-        res.writeHead(200);
-        res.end('end')
-        console.log('\n got name' + qs.parse(body).name + '\033[39m\n')
-    })
+    if('GET' === req.method && '/web/images' ==  req.url.substring(0,11)) {
+        fs.stat(__dirname + req.url ,function  (err,stat) {
+            if(err || !stat.isFile()) {
+                res.writeHead('404');
+                res.end('Not Found!');
+                return;
+            }
+            serve(__dirname + req.url ,'application/jpg')
+        })
+    } else if ('GET' == req.method &&  '/' == req.url) {
+        serve(__dirname + '/web/index.html','text/html')
+    } else {
+        res.writeHead(404);
+        res.end('Not Found!');
+    }
+
+    function serve (path,type) {
+        res.writeHead(200,{
+            'Contenet-type' : type
+        });
+        fs.createReadStream(path).pipe(res)
+    }
 
 }).listen(3000)
